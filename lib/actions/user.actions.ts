8806f5916d398bc714dc8db919679260c7b57838
h9nirajpatil <h9.nirajpatil@outlook.com>
2024-08-06 18:21:28 +0530

    implement footer done with profile icon

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -50,3 +50,15 @@ export async function getLoggedInUser() {
     return null;
   }
 }
+
+export const logoutAccount = async () => {
+  try {
+    const { account } = await createSessionClient();
+
+    cookies().delete("appwrite-session");
+
+    await account.deleteSession("current");
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
